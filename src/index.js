b2ea5bbbc22492caeeadb983e7245c173c12ffa4
Ryan Yogan <ryanyogan@gmail.com>
2018-03-17 13:10:51 -0700

    added in auth, create team mutation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from 'apollo-link-context';
 
 import 'semantic-ui-css/semantic.min.css';
 
@@ -11,12 +13,46 @@ import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:8080/graphql'
+  uri: 'http://localhost:8080/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('@slack-token');
+  const refreshToken = localStorage.getItem('@slack-refresh-token');
+
+  return {
+    headers: {
+      ...headers,
+      'x-token': token || '',
+      'x-refresh-token': refreshToken || '',
+    },
+  };
+});
+
+const afterwareLink = new ApolloLink((operation, forward) => {
+  const { headers } = operation.getContext();
+
+  if (headers) {
+    const token = headers.get('x-token');
+    const refreshToken = headers.get('x-refresh-token');
+
+    if (token) {
+      localStorage.setItem('@slack-token', token);
+    }
+
+    if (refreshToken) {
+      localStorage.setItem('@slack-refresh-token', refreshToken);
+    }
+  }
+
+  return forward(operation);
+});
+
+const link = afterwareLink.concat(authLink.concat(httpLink));
+
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+  link,
+  cache: new InMemoryCache(),
 });
 
 const App = (
---
+++ src/routes/CreateTeam.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import { Form, Container, Header, Message } from 'semantic-ui-react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class CreateTeam extends Component {
+  state = {
+    name: '',
+    nameError: '',
+  };
+
+  onChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+      nameError: '',
+    });
+  };
+
+  onSubmit = async e => {
+    e.preventDefault();
+
+    const { name } = this.state;
+    const response = await this.props.mutate({
+      variables: { name },
+    });
+    const { ok, errors } = response.data.createTeam;
+
+    if (ok) {
+      return this.props.history.push('/');
+    }
+
+    const err = {};
+    errors.forEach(({ path, message }) => {
+      err[`${path}Error`] = message;
+    });
+
+    this.setState(err);
+  };
+
+  render() {
+    const { name, nameError } = this.state;
+
+    const errorList = [nameError].filter(error => error.length && error);
+
+    return (
+      <Container text style={{ marginTop: '20px' }}>
+        <Header as="h2">Create Team</Header>
+        <Form onSubmit={this.onSubmit}>
+          <Form.Field error={!!nameError}>
+            <label htmlFor="name">Team Name</label>
+            <Form.Input
+              name="name"
+              type="text"
+              autoComplete="name"
+              onChange={this.onChange}
+              value={name}
+              placeholder="Team Name"
+            />
+          </Form.Field>
+          <Form.Button content="Submit" />
+        </Form>
+        {errorList.length ? (
+          <Message
+            error
+            header="There was an error during registration."
+            list={errorList}
+          />
+        ) : null}
+      </Container>
+    );
+  }
+}
+
+const createTeamMutation = gql`
+  mutation CreatTeam($name: String!) {
+    createTeam(name: $name) {
+      ok
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default graphql(createTeamMutation)(CreateTeam);
