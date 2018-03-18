0cbc0325afd9347403fca5160d599c1a78be0b08
Ryan Yogan <ryanyogan@gmail.com>
2018-03-18 11:09:00 -0700

    switched to styled components, added in small components, ViewTeam route

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,7 @@ import Home from './Home';
 import Register from './Register';
 import Login from './Login';
 import CreateTeam from './CreateTeam';
+import ViewTeam from './ViewTeam';
 
 import { isAuthenticated } from '../lib/auth';
 
@@ -37,6 +38,7 @@ export default () => (
       <Route path="/" exact component={Home} />
       <Route path="/register" exact component={Register} />
       <Route path="/login" exact component={Login} />
+      <Route path="/team" exact component={ViewTeam} />
       <ProtectedRoute path="/create-team" exact component={CreateTeam} />
     </Switch>
   </Router>
